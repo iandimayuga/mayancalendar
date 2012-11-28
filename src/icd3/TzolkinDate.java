7c768a86fb61fc7d1bddd455b4447fcaa81745d3
icd3 <git@iandimayuga.com>
2012-11-28 12:27:23 -0500

    Create TzolkinDate class stub

---
+++ src/icd3/TzolkinDate.java
@@ -0,0 +1,68 @@
+/**
+ *
+ */
+package icd3;
+
+/**
+ * Represents the Tzolkin method of Mayan calendaring.
+ */
+public class TzolkinDate implements MayanDate<TzolkinDate>
+{
+    /**
+     * Instantiates a TzolkinDate object from its textual representation.
+     * @param text The textual representation.
+     */
+    public TzolkinDate(String text)
+    {
+
+    }
+
+    /**
+     * Instantiates a TzolkinDate object from its integer representation.
+     * @param value The integer representation.
+     */
+    public TzolkinDate(int value)
+    {
+
+    }
+
+    /* (non-Javadoc)
+     * @see icd3.MayanDate#plus(int)
+     */
+    @Override
+    public TzolkinDate plus(int days)
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /* (non-Javadoc)
+     * @see icd3.MayanDate#minus(java.lang.Object)
+     */
+    @Override
+    public TzolkinDate minus(TzolkinDate other)
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /* (non-Javadoc)
+     * @see icd3.MayanDate#toInt()
+     */
+    @Override
+    public int toInt()
+    {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString()
+    {
+        return null;
+    }
+}
