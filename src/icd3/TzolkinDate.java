08a394d2af53b38750ce16903da5d91c895d9148
icd3 <git@iandimayuga.com>
2012-11-28 18:02:58 -0500

    Define Tzolkin date internal data

--- src/icd3/TzolkinDate.java
+++ src/icd3/TzolkinDate.java
@@ -3,13 +3,49 @@
  */
 package icd3;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Represents the Tzolkin method of Mayan calendaring.
  */
 public class TzolkinDate implements MayanDate<TzolkinDate>
 {
     /**
+     * The length of the cycle followed by the coefficients in a Tzolkin date.
+     */
+    private static final int s_numCoefficients = 13;
+
+    /**
+     * The day names in a Tzolkin date. The length of this array is the length of this second cycle.
+     */
+    private static final String[] s_dayNames = { "imix", "ik", "akbal", "kan", "chikchan", "kimi", "manik", "lamat",
+            "muluk", "ok", "chuen", "eb", "ben", "ix", "men", "kib", "kaban", "etznab", "kawak", "ajaw" };
+
+    /**
+     * Lookup table for day name representations
+     */
+    private static final Map<String, Integer> s_nameTable;
+
+    /**
+     * Initialize the lookup table for Date instantiation
+     */
+    static {
+        s_nameTable = new HashMap<>();
+        for (int i = 0; i < s_dayNames.length; ++i)
+        {
+            s_nameTable.put(s_dayNames[i], i);
+        }
+    }
+
+    /**
+     * Integer representation of this date
+     */
+    private int m_Value;
+
+    /**
      * Instantiates a TzolkinDate object from its textual representation.
+     *
      * @param text The textual representation.
      */
     public TzolkinDate(String text)
