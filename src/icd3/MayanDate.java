afe63a850b3fc00ace122a4e31f11f508f639515
icd3 <git@iandimayuga.com>
2012-11-28 04:49:22 -0500

    Create MayanDate interface

---
+++ src/icd3/MayanDate.java
@@ -0,0 +1,38 @@
+/**
+ *
+ */
+package icd3;
+
+/**
+ * An immutable data structure to represent a date in different Mayan calendars.
+ *
+ * The date representation is only compatible with other date representations of the same type.
+ */
+public interface MayanDate<T>
+{
+    /**
+     * Calculate a certain number of days away from this date.
+     * @param days The number of days to add (can be negative for subtract). If days is 0, this will return itself.
+     * @return A date object that is shifted from this date.
+     */
+    public T plus(int days);
+
+    /**
+     * Calculate the difference between this date and another date of the same type.
+     * @param other The other date of the same type.
+     * @return The difference between the two dates.
+     */
+    public T minus(T other);
+
+    /**
+     * Gives the internal integer representation of this date.
+     * @return The integer representation of this date.
+     */
+    public int toInt();
+
+    /**
+     * Gives the String representation of this date.
+     * @return The String representation of this date.
+     */
+    public String toString();
+}
